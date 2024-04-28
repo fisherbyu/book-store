552173c97e84ddf9f03b319cd6c370d706772ee2
fisherbyu <fisherandrew777@gmail.com>
2024-04-28 00:52:45 -0600

    Scaffold models

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.Models;
+
+public partial class Book
+{
+    [Key]
+    [Required]
+    public long BookId { get; set; }
+    public string Title { get; set; } = null!;
+    public string Author { get; set; } = null!;
+    public string Publisher { get; set; } = null!;
+    public string Isbn { get; set; } = null!;
+    public string Classification { get; set; } = null!;
+    public string Category { get; set; } = null!;
+    public long PageCount { get; set; }
+    public double Price { get; set; }
+}
